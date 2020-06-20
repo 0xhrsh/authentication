52610fbfd0eadcb4f4729635f2b1c3f69be565ff
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-06-20 20:50:57 +0530

    added time and time verification in auth token

--- models/token.js
+++ models/token.js
@@ -1,32 +1,46 @@
 var crypto = require("crypto");
-const {
-	timeStamp
-} = require("console");
+const {timeStamp} = require("console");
+
 const client = require("./client");
+
 var algorithm = "aes-192-cbc";
-var password = "Hello darkness";
+var password = "*insert_secret_key_here*";
+
 const key = crypto.scryptSync(password, 'salt', 24);
 const iv = Buffer.alloc(16, 0);
 
+const authWindow = 30 * 60 * 1000;
+
+
+// Class Token contains the implementation required for auth token operations,
+// including token creation and obtaining user details post credential verification
 function Token(authToken) {
 	this.authToken = authToken;
 }
 
 Token.prototype.generateToken = function (userID, clientID) {
 	const cipher = crypto.createCipheriv(algorithm, key, iv);
-	this.authToken = cipher.update(userID + "___" + clientID, 'utf8', 'hex') + cipher.final('hex');
+
+	let requestDate = new Date();
+	let infoString = userID + "___" + clientID + "___" + requestDate;
+
+	this.authToken = cipher.update(infoString, 'utf8', 'hex') + cipher.final('hex');
 };
 
 Token.prototype.getUserProfile = function (clientSecret) {
 	var decrytedToken = this.decrptyToken();
 	ldap = decrytedToken.split("___")[0];
 	clientID = decrytedToken.split("___")[1];
-	thisClient = new client(clientID, clientSecret, "");
-	if (thisClient.assertClientCreds()) {
-		// return getUser(ldap)
-	}
-	//return error
+	requestDate = new Date(decrytedToken.split("___")[2]).getTime();
 
+	if (new Date() <= new Date(requestDate + authWindow)) {
+		thisClient = new client(clientID, clientSecret, "");
+		if (thisClient.assertClientCreds()) {
+			// return getUser(ldap)
+		}
+		//return client invalid error
+	}
+	//return auth window expired error
 };
 
 
