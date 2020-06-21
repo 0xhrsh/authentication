6d7172662059bb3e77c36d2814ba2eac448e6248
YashviRamanuj <49070630+YashviRamanuj@users.noreply.github.com>
2020-06-21 18:25:31 +0530

    added class for client

--- models/client.js
+++ models/client.js
@@ -1,12 +1,71 @@
-function Client(clientID, clientSecret, redirectURL) {
-	this.clientID = clientID;
-	this.clientSecret = clientSecret;
-	this.redirectURL = redirectURL;
+const SQLiteClient = require('./SQLiteClient');
+const dbclient = new SQLiteClient('./auth.db');
+
+class Client{
+	constructor(clientID, clientSecret, redirectURL){
+		this.client_id = clientID;
+		this.client_secret = clientSecret;
+		this.redirect_uri = redirectURL;
+	}
+
+	register(){
+		dbclient.insertIntoTable('clients', this);
+	}
+
+	static fetchFromDB(clientID){
+		return new Promise(async (resolve, reject) => {
+			if (await dbclient.exist('clients', 'client_id', clientID)) {
+				const user = await dbclient.getFromTable('clients', 'client_id', clientID);
+				resolve(new Client(
+					client.client_id,
+					client.client_secret,
+					client.redirect_uri, 
+				));
+			} else {
+				resolve(false);
+			}
+		});
+	}
+
+	static exist(clientID){
+		return new Promise((resolve, reject) => {
+			dbclient.exist('clients', 'client_id', clientID)
+				.then(exist => resolve(exist))
+				.catch(err => reject(err));
+		});
+	}
+
+	static assertCreds(clientID, clientSecret){
+		return new Promise((resolve, reject) => {
+			dbclient.getFromTable('clients' , 'client_id' , clientID)
+				.then(res => {
+					if(clientSecret === res.client_secret){
+						resolve(true);
+					} else {
+						resolve(false);
+					}
+				})
+				.catch (err => {
+					reject(err);
+				});
+		});
+	}
 }
 
-Client.prototype.assertClientCreds = function () {
-	// match credentials from db
-	return true;
-};
+// const client = new Client("abc", "def", "ghi")
+// async function main(){
+// 	client.register();
+//     var obj = await Client.fetchFromDB("abc");
+//     console.log(obj.client_id);
+//     var p=Client.assertCreds("abc", "def");
+//     p.then(function(val) { 
+//         console.log(val); 
+//     });
+//     var p=Client.exist("abc");
+//     p.then(function(val) { 
+//         console.log(val); 
+//     }); 	
+// }
+// main();
 
 module.exports = Client;
